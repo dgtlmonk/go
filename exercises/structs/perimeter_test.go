60c499b69118fdb6d461adf75661392fcf38e16d
dgtlmonk <dgtlmonk@gmail.com>
2019-08-11 21:06:58 +0800

    introduce table test driven

--- exercises/structs/perimeter_test.go
+++ exercises/structs/perimeter_test.go
@@ -35,3 +35,27 @@ func TestPerimeter(t *testing.T) {
 		checkArea(t, circle, want)
 	})
 }
+
+/*
+Table Test Driven
+
+new syntax here is creating an "anonymous struct", areaTests.
+We are declaring a slice of structs by using []struct with two fields,
+the shape and the want. Then we fill the slice with cases.
+*/
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
+}
