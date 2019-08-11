f1b9bbb648741018c5dada44fa4c5153a975f2e4
dgtlmonk <dgtlmonk@gmail.com>
2019-08-11 19:02:47 +0800

    refactor DRY test func

--- exercises/arrays/arrays_test.go
+++ exercises/arrays/arrays_test.go
@@ -11,10 +11,20 @@ func TestSum(t *testing.T) {
 		got := Sum(num)
 		want := 15
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, num)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
 		}
 	})
+
+}
+
+func TestSlices(t *testing.T) {
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
 
 	t.Run("sum of two slices", func(t *testing.T) {
 		slice1 := []int{2, 10}
@@ -23,27 +33,21 @@ func TestSum(t *testing.T) {
 		got := SumAll(slice1, slice2)
 		want := []int{12, 12}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("sum of all tails", func(t *testing.T) {
 		got := SumAllTails([]int{1, 2}, []int{0, 9})
 		want := []int{2, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		want := []int{0, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		checkSums(t, got, want)
 	})
 }
 
